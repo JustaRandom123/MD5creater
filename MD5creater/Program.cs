49fc4675293d1ed03bb62d3289d357c9f6ca7679
JustaRandom123 <47950815+JustaRandom123@users.noreply.github.com>
2022-02-21 00:00:31 +0100

    Add files via upload

---
+++ MD5creater/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD5creater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+          
+            string[] files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*", SearchOption.AllDirectories);
+
+            Console.WriteLine("Pfad: " + AppDomain.CurrentDomain.BaseDirectory);
+
+            foreach (var fileName in files)
+            {
+                Console.WriteLine(fileName + "   Hash: " + CalculateMD5(fileName));
+            }
+
+            Console.ReadLine();
+        }
+
+        static string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+    }
+
+   
+
+   
+}
